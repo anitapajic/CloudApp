77e6c8a55c672dee234ce436dfb0839a95d129cb
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-05-04 22:34:57 +0200

    Registration

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CognitoService } from '../services/cognito.service';
+import { Route, Router } from '@angular/router';
+import { IUser } from '../model/User';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +10,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit{
+  isCreated : boolean = false;
+  userForm!: FormGroup;
+
+  constructor(private router: Router, private cognitoService : CognitoService){}
 
   ngOnInit() {
     const signUpButton = document.getElementById('signUp') as HTMLElement | any;
@@ -19,6 +27,34 @@ export class LoginComponent implements OnInit{
     signInButton.addEventListener('click', () => {
       container.classList.remove("right-panel-active");
     });
+    this.userForm = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+      code: new FormControl('')
+    });
+  }
+
+  signUp() {
+    const user: IUser = this.userForm.value;
+    console.log(user)
+    this.cognitoService.signUp(user).then(()=>{
+      this.isCreated = true;
+    }).catch((error) =>{
+        alert(error)
+    } )
+  }
+
+  verify() {
+    const user: IUser = this.userForm.value;
+    this.cognitoService.verify(user).then(()=>{
+      this.isCreated = false;
+    }).catch((error) =>{
+        alert(error)
+    } )
+
+  }
+
+  signIn() {
   }
 
 }
---
+++ src/app/services/cognito.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../model/User';
+import { Amplify, Auth } from 'aws-amplify';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CognitoService {
+ 
+
+  constructor() {
+    Amplify.configure({
+      Auth:environment.cognito
+    })
+   }
+
+  signUp(user: IUser):Promise<any>{
+    return Auth.signUp({
+      username:user.username,
+      password:user.password
+    })
+   }
+  verify(user: IUser):Promise<any> {
+    return Auth.confirmSignUp(user.username,user.code);
+  }
+
+}
