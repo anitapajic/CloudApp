b75f20810d01b2bcd9b5a22bcc10ec129651814e
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-05-05 20:02:38 +0200

    Added drag and drop

--- src/app/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file.component.ts
@@ -11,7 +11,7 @@ import { IFile } from '../model/File';
 export class UploadFileComponent implements OnInit{
 
 
-  file!:File;
+  file:File| null = null;
   myFile: IFile = {} as IFile;
 
   constructor(private fileService: FileService) { }
@@ -19,23 +19,53 @@ export class UploadFileComponent implements OnInit{
   ngOnInit() {
     console.log('Upload initialised');
   }
+
+  handleDragOver(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    event.dataTransfer.dropEffect = 'copy';
+    document.getElementById('file-drag')!.classList.add('hover');
+  }
+  
+  handleDragLeave(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    document.getElementById('file-drag')!.classList.remove('hover');
+
+
+  }
+  
+  handleFileSelect(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+    document.getElementById('file-drag')!.classList.remove('hover');
+  
+    this.file = event.dataTransfer.files[0];
+    this.createMyFile();
+  }
+
+
   onFileSelected(event: any) {
     this.file = event.target.files[0];
+    this.createMyFile();
+  }
 
-    this.myFile.file = this.file;
-    this.myFile.name = this.file.name;
-    this.myFile.type = this.file.type;
-    this.myFile.date = new Date();
-    this.myFile.modified = this.myFile.date;
-    this.myFile.size = this.file.size / (1024 * 1024);
-  
-
-    // Do something with the selected file, such as upload it to your server
+  createMyFile(){
+    if (this.file != null) {
+      this.myFile.file = this.file;
+      this.myFile.name = this.file.name;
+      this.myFile.type = this.file.type;
+      this.myFile.date = new Date();
+      this.myFile.modified = this.myFile.date;
+      this.myFile.size = this.file.size / (1024 * 1024);
+    }
   }
 
   uploadFile() {
     this.fileService.uploadFile(this.myFile);
-    console.log('Successfully uploaded')
+    console.log('Successfully uploaded');
+    this.file = null;
+    this.myFile = {} as IFile;
   }
   
 }
