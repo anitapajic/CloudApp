378b59b8a50a822f9c6f7a112a9802e2306ba595
Tamara <dzambic.sv43.2020@uns.ac.rs>
2023-05-06 15:24:18 +0200

    Small fix

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { IFile } from '../model/File';
 import { environment } from 'src/environments/environment';
 import { CognitoService } from './cognito.service';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +13,7 @@ export class FileService {
   constructor(private cognito: CognitoService, private http:HttpClient) { }
 
 
+  
   async uploadFile(file : IFile){
 
 
@@ -22,13 +23,42 @@ export class FileService {
     var typeFolder = this.findFolder(file.type.split('/')[0]);
 
     var fileDir = '/' + file.username + '/' + typeFolder + '/' + file.name
-    this.http.put<any>(this.s3_path + fileDir , file.file).subscribe((response)=>{
-      console.log(response);
-    });
+
+    console.log(this.s3_path + fileDir);
+
+    console.log(file.file)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+
+    // this.http.options<any>(' https://ihsotip46f.execute-api.us-east-1.amazonaws.com/dev/tim19-bucket/tamara_dzambic@hotmail.com/documents/aaasdc.pdf').subscribe(
+    //   (response) => {
+    //     console.log("Options1 ",response);
+    //   },
+    //   (error) => {
+    //     console.error("Options2 ",error);
+    //   }
+    // )
+    this.http.put<File>(this.s3_path + fileDir , file.file, httpOptions).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
     if(file.favourite){
       fileDir = '/' + file.username + '/favourites/' + file.name
-      // response = this.http.put<any>(this.s3_path + fileDir , file.file);
-      // console.log(response);
+      this.http.put<any>(this.s3_path + fileDir , file.file).subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
 
     }
 
